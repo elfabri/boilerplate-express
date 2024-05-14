05272356b84272cbf696185766510281cdd963b0
Fabricio Navarro <navarro.fabricio.nahuel@gmail.com>
2024-05-14 02:41:20 +0000

    Basic node and express 5/12

--- myApp.js
+++ myApp.js
@@ -1,8 +1,20 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+let indexPath = __dirname + "/views/index.html";
+let pubPath = __dirname + "/public";
 
+app.get("/", function(req, res){
+    res.sendFile(indexPath);
+})
+
+app.use("/public", express.static(pubPath));
+
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"});
+})
 
 
 
